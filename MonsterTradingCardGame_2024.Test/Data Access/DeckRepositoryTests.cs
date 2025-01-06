964661b8b9130d673af8262a2aa3eb5ea4fbc1ce
CaptainKnobel <cklein@live.at>
2025-01-06 19:26:17 +0100

    killed unnecessary tests scripts

--- MonsterTradingCardGame_2024.Test/Data Access/DeckRepositoryTests.cs
+++ MonsterTradingCardGame_2024.Test/Data Access/DeckRepositoryTests.cs
@@ -10,7 +10,7 @@ using Npgsql;
 using NUnit.Framework;
 
 namespace MonsterTradingCardGame_2024.Test.Data_Access
-{
+{/*
     [TestFixture]
     public class DeckRepositoryTests : IDisposable
     {
@@ -81,5 +81,5 @@ namespace MonsterTradingCardGame_2024.Test.Data_Access
             _transaction?.Dispose();
             _connection?.Dispose();
         }
-    }
+    }*/
 }
